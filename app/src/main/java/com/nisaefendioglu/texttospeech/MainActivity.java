ed684a1dc1612591dff5271b81c4a386ac0ee958
Nisa Efendioğlu <nisaefendioglu0@gmail.com>
2022-02-23 13:08:05 +0300

    TextToSpeech.

--- app/src/main/java/com/nisaefendioglu/texttospeech/MainActivity.java
+++ app/src/main/java/com/nisaefendioglu/texttospeech/MainActivity.java
@@ -3,12 +3,51 @@ package com.nisaefendioglu.texttospeech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech t1;
+    EditText ed1;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ed1=(EditText)findViewById(R.id.editText);
+        b1=(Button)findViewById(R.id.button);
+        Locale locale = new Locale("tr", "TR");
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(locale);
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = ed1.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
     }
 }
